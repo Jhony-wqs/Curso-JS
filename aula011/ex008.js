3e175b30752dff07c695818637b02583eebd1ce2
Jhony-wqs <jhony270288@gmail.com>
2023-05-19 10:00:18 -0300

    exercício

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`Sua velocidade foi de ${vel}km/hr`)
+if(vel > 60){
+    console.log('Voçê esta acima da velocidade permitida voçê foi MULTDO!')
+}
+console.log('Dirija sempre usando o cinto de segurança ele salva vidas!')
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais ='eua'
+if(pais == 'Brasil'){
+    console.log('voçê e brasileiro!')
+} else{
+    console.log('voçê e estrangeiro!')
+}
+console.log('Qualquer forma seja bem vindo!!!')
