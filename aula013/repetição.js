db777cd3aca079f1eef9bdbf10ab171c53f1f33d
Jhony-wqs <jhony270288@gmail.com>
2023-07-03 11:42:34 -0300

    exericio

---
+++ aula013/repetição.js
@@ -0,0 +1,14 @@
+/*var c = 1
+while (c <= 6){
+    console.log('jhony')
+    c++
+}
+var c =1 
+do{
+   console.log(`passo ${c}`)
+   c++
+}while(c <=6)
+*/
+for(c=1;c<=6;c++){
+    console.log(`passo ${c}`)
+}
